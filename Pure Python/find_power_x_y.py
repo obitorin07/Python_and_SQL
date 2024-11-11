f52e2872a66c4e98592e766a22b83d36a386b83c
Obito Uchiha <157777144+obitorin07@users.noreply.github.com>
2024-11-11 10:13:18 +0530

    Create find_power_x_y.py
    this is leetcode problem to return power of  x ** y

---
+++ Pure Python/find_power_x_y.py
@@ -0,0 +1,10 @@
+# return power of x y
+# example 2 power 3 2*2*2 = 8
+
+def power_x_y(x , y):
+    return f'The Power of {x} and {y} is : {x ** y}'
+
+
+print(power_x_y(2,3))
+print(power_x_y(2,4.15))
+print(power_x_y(3.2,3))
